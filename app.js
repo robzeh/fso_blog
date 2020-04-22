57e24652f7400732b52e2d627761e105ab76e162
robzeh <jn912820@dal.ca>
2020-04-22 12:48:55 -0300

    Refactored into express router

--- app.js
+++ app.js
@@ -5,8 +5,9 @@ const mongoose = require("mongoose");
 
 const config = require("./utils/config");
 const logger = require("./utils/logger");
+const middleware = require("./utils/middleware");
 
-const Blog = require("./models/blog");
+const blogRouter = require("./controllers/blogs");
 
 const mongoUrl = config.MONGODB_URL;
 mongoose
@@ -20,19 +21,11 @@ mongoose
 
 app.use(cors());
 app.use(express.json());
+app.use(middleware.requestLogger);
 
-app.get("/api/blogs", (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs);
-  });
-});
+app.use("/", blogRouter);
 
-app.post("/api/blogs", (request, response) => {
-  const blog = new Blog(request.body);
-
-  blog.save().then((result) => {
-    response.status(201).json(result);
-  });
-});
+app.use(middleware.errorHandler);
+app.use(middleware.unknownEndpoint);
 
 module.exports = app;
