c93f5a5c62990fa4492669caa9fc22b585a95311
robzeh <jn912820@dal.ca>
2020-04-21 21:19:29 -0300

    initial commit

---
+++ app.js
@@ -0,0 +1,38 @@
+const http = require("http");
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const mongoose = require("mongoose");
+
+const blogSchema = mongoose.Schema({
+  title: String,
+  author: String,
+  url: String,
+  likes: Number,
+});
+
+const Blog = mongoose.model("Blog", blogSchema);
+
+// mongodb+srv://fullstack:fullstack@cluster0-rxwei.mongodb.net/test?retryWrites=true&w=majority
+const mongoUrl =
+  "mongodb+srv://fullstack:fullstack@cluster0-rxwei.mongodb.net/test?retryWrites=true&w=majority";
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/api/blogs", (request, response) => {
+  Blog.find({}).then((blogs) => {
+    response.json(blogs);
+  });
+});
+
+app.post("/api/blogs", (request, response) => {
+  const blog = new Blog(request.body);
+
+  blog.save().then((result) => {
+    response.status(201).json(result);
+  });
+});
+
+module.exports = app;
