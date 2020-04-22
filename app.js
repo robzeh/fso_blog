2619bd206edc202c85d7929c237e0e56052bca6a
robzeh <jn912820@dal.ca>
2020-04-22 12:24:32 -0300

    Refactored Blog model

--- app.js
+++ app.js
@@ -6,14 +6,7 @@ const mongoose = require("mongoose");
 const config = require("./utils/config");
 const logger = require("./utils/logger");
 
-const blogSchema = mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number,
-});
-
-const Blog = mongoose.model("Blog", blogSchema);
+const Blog = require("./models/blog");
 
 const mongoUrl = config.MONGODB_URL;
 mongoose
