6f262d2f310d24e55a7be1be88e065032accbbd2
robzeh <jn912820@dal.ca>
2020-04-22 12:17:56 -0300

    Refactored utils folder

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
-const http = require("http");
 const express = require("express");
 const app = express();
 const cors = require("cors");
 const mongoose = require("mongoose");
+
+const config = require("./utils/config");
+const logger = require("./utils/logger");
 
 const blogSchema = mongoose.Schema({
   title: String,
@@ -13,10 +15,15 @@ const blogSchema = mongoose.Schema({
 
 const Blog = mongoose.model("Blog", blogSchema);
 
-// mongodb+srv://fullstack:fullstack@cluster0-rxwei.mongodb.net/test?retryWrites=true&w=majority
-const mongoUrl =
-  "mongodb+srv://fullstack:fullstack@cluster0-rxwei.mongodb.net/test?retryWrites=true&w=majority";
-mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
+const mongoUrl = config.MONGODB_URL;
+mongoose
+  .connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    logger.info("Connected to MongoDB");
+  })
+  .catch((error) => {
+    logger.error(`Error: ${error.message}`);
+  });
 
 app.use(cors());
 app.use(express.json());
--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const app = require("./app");
 const http = require("http");
+const logger = require("./utils/logger");
+const config = require("./utils/config");
 
 const server = http.createServer(app);
 
-server.listen(3003, () => {
-  console.log(`Server running on port 3003`);
+server.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`);
 });
